a51b871589f778645aa1156299c19f852a6bc2df
amolica <andrewmolica@gmail.com>
2016-10-24 12:07:38 +0400

    Initial commit

--- src/main/java/edu/aca/onlineshop/backoffice/order/OrderDAOImp.java
+++ src/main/java/edu/aca/onlineshop/backoffice/order/OrderDAOImp.java
@@ -1,14 +1,26 @@
 package edu.aca.onlineshop.backoffice.order;
 
 import edu.aca.onlineshop.backoffice.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
 /**
  *
  */
+@Repository
 public class OrderDAOImp implements OrderDAO{
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
+    public JdbcTemplate getJdbcTemplate(){return jdbcTemplate;}
+    
     @Override
     public List<Order> getOrders(){
         return null;
@@ -42,5 +54,25 @@ public class OrderDAOImp implements OrderDAO{
     @Override
     public void addOrder(Order order){
         
+    }
+}
+
+class OrderOrderListRowMapper implements RowMapper<Order>{
+    @Override
+    public Order mapRow(ResultSet resultSet, int i) throws SQLException{
+        Order order = new Order();
+        order.setId(resultSet.getInt("id"));
+        order.setUserId(resultSet.getInt("user_id"));
+        order.setAmount(resultSet.getBigDecimal("amount"));
+        order.setPurchaseDate(resultSet.getTimestamp("purchase_date"));
+        
+        switch(resultSet.getInt("status")){
+            case 1: order.setOrderStatus(OrderStatus.ORDERED);break;
+            case 2: order.setOrderStatus(OrderStatus.SHIPPED);break;
+            case 3: order.setOrderStatus(OrderStatus.DELIVERED);break;
+        }
+        
+        order.setDeliveryDate(resultSet.getTimestamp("delivery_date"));
+        return order;
     }
 }
