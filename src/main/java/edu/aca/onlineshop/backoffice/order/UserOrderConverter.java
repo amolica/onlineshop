09009459f9dff93b4201edabacb99c6bc06c2184
amolica <andrewmolica@gmail.com>
2016-10-24 11:58:23 +0400

    Converts UserOrder to Order

--- src/main/java/edu/aca/onlineshop/backoffice/order/UserOrderConverter.java
+++ src/main/java/edu/aca/onlineshop/backoffice/order/UserOrderConverter.java
@@ -1,7 +1,29 @@
 package edu.aca.onlineshop.backoffice.order;
 
+
+import edu.aca.onlineshop.backoffice.product.Product;
+import edu.aca.onlineshop.backoffice.product.ShopProduct;
+import edu.aca.onlineshop.backoffice.product.ShopProductConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  */
 public class UserOrderConverter{
+    public static Order convertToOrder(UserOrder userOrder){
+        Order order = new Order();
+        order.setUserId(userOrder.getUserId());
+        order.setAmount(userOrder.getAmount());
+        order.setPurchaseDate(userOrder.getPurchaseDate());
+        order.setOrderStatus(userOrder.getOrderStatus());
+        order.setDeliveryDate(userOrder.getDeliveryDate());
+        List<Product> products = new ArrayList<>();
+        for(ShopProduct s : userOrder.getShopProducts()){
+            products.add(ShopProductConverter.convertToProduct(s));
+        }
+        order.setProducts(products);
+        return order;
+    }
 }
