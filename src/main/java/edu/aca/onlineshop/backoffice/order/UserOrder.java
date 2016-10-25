095bc58e74da09c2c7b802ac764783d4bfb2922d
amolica <andrewmolica@gmail.com>
2016-10-25 16:45:50 +0400

    Added methods that allow order to be purchased

--- src/main/java/edu/aca/onlineshop/backoffice/order/UserOrder.java
+++ src/main/java/edu/aca/onlineshop/backoffice/order/UserOrder.java
@@ -1,16 +1,19 @@
 package edu.aca.onlineshop.backoffice.order;
 
 import edu.aca.onlineshop.backoffice.product.Product;
-import edu.aca.onlineshop.backoffice.product.ShopProduct;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
  */
 public class UserOrder {
+    private static Scanner scanner = new Scanner(System.in);
+    
     private int userId;
     private BigDecimal amount;
     private Timestamp purchaseDate;
@@ -20,6 +23,34 @@ public class UserOrder {
     
     public UserOrder(){
         this.products = new ArrayList<>();
+    }
+    
+    //can add formatting later
+    public void viewProductsInOrder(){
+        System.out.println(products);
+    }
+    
+    public void finalizeOrder(){
+        this.purchaseDate = Timestamp.from(Instant.now());
+        this.orderStatus = OrderStatus.ORDERED;
+        for(Product p : products){
+            amount.add(p.getPrice());
+        }
+        this.deliveryDate = chooseDeliveryDate();
+    }
+    
+    private Timestamp chooseDeliveryDate(){
+        System.out.println("At OnlineShop we only offer next day delivery because it makes our lives easier.");
+        System.out.println("Enter the hour (24hr format) when you wish to have your order delivered:");
+        int hour = scanner.nextInt();
+        Timestamp timestamp = Timestamp.from(Instant.now());
+        //change day to tomorrow
+        timestamp.setDate(timestamp.getDay()+1);
+        timestamp.setHours(hour);
+        timestamp.setMinutes(0);
+        timestamp.setSeconds(0);
+        timestamp.setNanos(0);
+        return timestamp;
     }
     
     public int getUserId(){
