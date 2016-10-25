4912ffdfd90ff3eca6b26a33e95475824d5eb4b5
amolica <andrewmolica@gmail.com>
2016-10-25 12:42:22 +0400

    Creates product from user input

---
+++ src/main/java/edu/aca/onlineshop/backoffice/product/ProductInfoForm.java
@@ -0,0 +1,35 @@
+package edu.aca.onlineshop.backoffice.product;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class ProductInfoForm{
+    private static Scanner scanner = new Scanner(System.in);
+    private ShopProduct shopProduct;
+    
+    public void createProduct(){
+        this.shopProduct = new ShopProduct(collectName(), collectPrice(), collectQuantity());
+    }
+    
+    private String collectName(){
+        System.out.println("Enter product name:");
+        return scanner.next();
+    }
+    
+    private BigDecimal collectPrice(){
+        System.out.println("Enter product price:");
+        return scanner.nextBigDecimal();
+    }
+    
+    private int collectQuantity(){
+        System.out.println("Enter product quantity:");
+        return scanner.nextInt();
+    }
+    
+    public ShopProduct getShopProduct(){
+        return shopProduct;
+    }
+}
